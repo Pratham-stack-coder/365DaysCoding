8c8db629f183b9398f7cf778039fd8eb0fc9efa3
Pratham Gupta <prathamgupta0070@gmail.com>
2026-01-21 09:53:38 +0530

    Add code2.cpp for user input and output example

---
+++ First Day/First Code + User Input And Output/code2.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Value Of X: " << x << endl << "Value of y: " << y;
+    return 0;
+}
+// #include <bits/stdc++.h>
+// It includes all the libraries in C++ 
