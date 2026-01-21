d01dce4e756c4c29f3d8970f59c48ea656dca810
Pratham Gupta <prathamgupta0070@gmail.com>
2026-01-21 13:28:29 +0530

    Add basic while and do-while loop example in code.cpp

---
+++ First Day/while loop basic/code.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // while loop
+    int i = 1;
+    while(i<=5){
+        cout << "Pratham\n";
+        i = i+1;
+    }
+    // do while loop 
+    int x = 2;
+    do {
+        cout << "Pratham " << x << endl;
+        x = x+1;
+    }while(x<=1);
+    cout << x << endl;
+    return 0;
+}
