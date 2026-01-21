af8bdf92cdbfb27e52ace2641871c0251dcd43fb
Pratham Gupta <prathamgupta0070@gmail.com>
2026-01-21 16:33:01 +0530

    Add printName function to greet user
    Implement a simple function to print a name.

---
+++ First Day/Functions/void/code.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Functions are set of codes which performs something for you.
+// Functions are used to modularise code.
+// Functions are used to increase readibility
+// Functions are used to use same code multiple times
+// void -> which does not return anything
+// return
+// parameterised 
+// non - parameterised
+void printName(string name){
+    cout << "hey " << name;
+}
+int main() {
+    string name;
+    cin >> name;
+    printName(name);
+    return 0;
+}
